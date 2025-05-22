f06ad8152c21a1dfd1bdfe0abcbec2f24c49075b
kema-dev <github@kema.dev>
2025-05-22 21:41:18 +0200

    feat: improve VPN interface command in kema_theme.omp.yaml and host_get_infos

--- dotfiles/scripts/host_get_infos.sh
+++ dotfiles/scripts/host_get_infos.sh
@@ -16,20 +16,20 @@ function main() {
 	local reset="\033[0m"
 	echo -e "${blue}Testing reachability of ${host}${reset}"
 	echo -e "${blue}host ${host}${reset}"
-	host "${host}"
+	host "${host}" || true
 	echo -e "${blue}nslookup ${host}${reset}"
-	nslookup "${host}"
+	nslookup "${host}" || true
 	echo -e "${blue}dog ${host}${reset}"
-	doggo "${host}"
+	doggo "${host}" || true
 	echo -e "${blue}traceroute -m 15 ${host}${reset}"
-	traceroute -m 15 "${host}"
+	traceroute -m 15 "${host}" || true
 	echo -e "${blue}ping -c 5 ${host}${reset}"
 	ping -c 5 -W 5 "${host}" || true
 	if [[ "${no_scan}" == "true" ]]; then
 		return 0
 	fi
 	echo -e "${blue}nmap -T5 -sC -sV -Pn --max-retries 3 --host-timeout 15s -p 20,21,22,23,25,53,80,110,143,389,443,465,587,3306,5432,8080,8081,8443,27017 ${host}${reset}"
-	nmap -T5 -sC -sV -Pn --max-retries 3 --host-timeout 15s -p 20,21,22,23,25,53,80,110,143,389,443,465,587,3306,5432,8080,8081,8443,27017 "${host}"
+	nmap -T5 -sC -sV -Pn --max-retries 3 --host-timeout 15s -p 20,21,22,23,25,53,80,110,143,389,443,465,587,3306,5432,8080,8081,8443,27017,51820 "${host}"
 }
 
 main "${@}"
