3118470d0f4f7e01806d6ce9e8a3bf48197f99ed
Jérémy Jourdan <github@kema.dev>
2025-07-17 12:59:51 +0200

    feat(fzf): update custom fzf flags for fzf-tab with new color options

--- dotfiles/shell/zsh/plugins/kema_misc.zsh
+++ dotfiles/shell/zsh/plugins/kema_misc.zsh
@@ -42,7 +42,7 @@ zstyle ':completion:*:match:*' original true
 # preview processes
 zstyle ':completion:*:*:*:*:processes' command 'command ps -u ${USER} -o pid,user,comm -w -w'
 
-# custom fzf flags
+# custom fzf flags for fzf-tab (from ${FZF_COLOR_OPTS})
 zstyle ':fzf-tab:*' fzf-flags --color=fg:#4d4d4c,bg:#eff1f5,hl:#d7005f --color=fg+:#4d4d4c,bg+:#e8e8e8,hl+:#d7005f --color=info:#4271ae,prompt:#8959a8,pointer:#d7005f --color=marker:#4271ae,spinner:#4271ae,header:#4271ae
 
 # switch group using `[` and `]`
